aa2acac018ed291170fc8212db5badab735ff062
Anderson Irias <anderson.irias@mav.com.br>
2020-11-03 21:28:48 -0300

    Components Header and PromotionFormSearch
    Components Header and PromotionFormSearch. See  about in
    https://irias.com.br/blog/criando-o-primeiro-componente-com-react-native/

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native'
+
+import Header from './components/Header'
 
 const App: () => React$Node = () => {
   return (
     <>
+      <StatusBar />
+      <SafeAreaView>
+        <Header />
+      </SafeAreaView>
     </>
   );
 };
