221bba06e7b9262a554060c0e19b42bf82e35919
Anderson Irias <anderson.irias@mav.com.br>
2020-11-21 14:34:08 -0300

    Redux Reducers

---
+++ reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import promotions from './promotions'
+import promotionPage from './promotionPage'
+
+export default combineReducers({
+  promotions,
+  promotionPage
+})
