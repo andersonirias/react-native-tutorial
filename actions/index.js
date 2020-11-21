71cdb15a9b58357d42f2356b28e86998dd75ffb1
Anderson Irias <anderson.irias@mav.com.br>
2020-11-21 14:32:43 -0300

    Redux Actions

---
+++ actions/index.js
@@ -0,0 +1,16 @@
+export const addPromotion = data => ({
+  type: 'ADD_PROMOTION',
+  data
+})
+
+export const clearPromotions = {
+  type: 'CLEAR_PROMOTIONS'
+}
+
+export const pageIncrement = {
+  type: 'INCREMENT'
+}
+
+export const pageBackToStart = {
+  type: 'BACK_TO_START'
+}
